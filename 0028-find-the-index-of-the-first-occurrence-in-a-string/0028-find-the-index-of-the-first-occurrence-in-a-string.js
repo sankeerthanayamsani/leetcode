a4815430eabffd4e6082f92b926e3280a4e4e6a5
sankeerthanayamsani <sankeerthanayamsani@gmail.com>
2025-09-11 20:40:09 +0530

    Time: 0 ms (100%), Space: 55 MB (19.95%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -6,17 +6,42 @@
 var strStr = function(haystack, needle) {
     let n = haystack.length;
     let m = needle.length;
-    for(let i=0;i<=n-m;i++){
-        let j=0;
-        // check if needle === window
-        for(j=0;j<m;j++){
-            if(haystack[i+j]!==needle[j]){
-                break;
+    let lps = [0];
+    let i=0;
+    let j =1;
+    while(j<m){
+        if(needle[i]===needle[j]){
+            lps[j] = i+1;
+            ++i;++j;
+        }
+        else{
+            if(i==0){
+                lps[j]=0;
+                ++j;
+            }
+            else{
+                i=lps[i-1];
             }
         }
-    if (j===m){
-            return i;
-        }
     }
-    return -1
+    i=j=0;
+    while(i<n){
+        if(haystack[i]===needle[j]){
+            ++i;++j;
+        }
+        else{
+            if(j==0){
+                ++i;
+            }
+            else{
+                j=lps[j-1];
+            }
+        }
+            if(j===m){
+                return i-m;
+            }
+        }
+        return -1;
+
+    
 };
