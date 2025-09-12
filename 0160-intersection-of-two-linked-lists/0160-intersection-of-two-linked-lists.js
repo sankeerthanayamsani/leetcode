8aa78d972a0b5cca94f09d228e942a7f881eede5
sankeerthanayamsani <sankeerthanayamsani@gmail.com>
2025-09-12 18:46:31 +0530

    Time: 79 ms (7.77%), Space: 63.4 MB (82%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
@@ -12,12 +12,34 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function(headA, headB) {
-    let pA=headA;
-    let pB=headB;
+    let n=0;
+    let pA = headA;
+    while(pA){
+        ++n;
+        pA = pA.next;
+    }
+    let m=0;
+    let pB = headB;
+    while(pB){
+        ++m;
+        pB = pB.next;
+    }
+    let diff = Math.abs(n-m);
+    //i want this first list as small,second is large
+    if(n>m){
+        temp = headA;
+        headA=headB;
+        headB=temp;
+    }
+    //list as small,second is large
+    for(let i =0;i<diff;i++){
+        headB=headB.next;
+    }
+    pA=headA;
+    pB=headB;
     while(pA!=pB){
-        pA = pA == null ? headB : pA.next;
-        pB = pB == null ? headA : pB.next;
+        pA=pA.next;
+        pB=pB.next;
     }
     return pA;
-    
 };
